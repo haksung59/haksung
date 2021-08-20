c65a3ff695cd89628bacfbb11b733bd7aa07801c
haksung <hak9501@gmail.com>
2021-08-20 14:45:36 +0900

    practice 21.08.20

--- practiceProj/src/main/java/aaa/practiceProjApplication.java
+++ practiceProj/src/main/java/aaa/practiceProjApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class practiceProjApplication {
+public class PracticeProjApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(practiceProjApplication.class, args);
+		SpringApplication.run(PracticeProjApplication.class, args);
 	}
 
 }
