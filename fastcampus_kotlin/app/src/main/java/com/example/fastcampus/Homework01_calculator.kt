056e117a6a1f54340678fcd7259252093b7314e0
haksung59 <hak9501@gmail.com>
2022-05-22 20:10:03 +0900

    android homework upload

--- fastcampus_kotlin/app/src/main/java/com/example/fastcampus/Homework01_calculator.kt
+++ fastcampus_kotlin/app/src/main/java/com/example/fastcampus/Homework01_calculator.kt
@@ -2,6 +2,7 @@ package com.example.fastcampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -12,6 +13,7 @@ class Homework01_calculator : AppCompatActivity() {
 
         var input1 : String = ""
         var input2 : String = ""
+        var result : String = ""
         var operator : String = ""
 
         var textViewResult : TextView = findViewById(R.id.calResult)
@@ -30,44 +32,112 @@ class Homework01_calculator : AppCompatActivity() {
         val buttonEqual : TextView = findViewById(R.id.equal_button)
 
         buttonOne.setOnClickListener{
-//            if(isInt(result)) {
-//                result += "1"
-//                textViewResult.setText(result)
-//            }else {
-//                result += "1"
-//                textViewResult.setText("1")
-//            }
+            if(operator=="") {
+                input1 = setInput(input1,"1")
+            }else {
+                input2 = setInput(input2,"1")
+            }
         }
         buttonTwo.setOnClickListener{
-            textViewResult.setText("2")
+            if(operator=="") {
+                input1 = setInput(input1, "2")
+            }else {
+                input2 = setInput(input2, "2")
+            }
         }
         buttonThree.setOnClickListener{
-            textViewResult.setText("3")
+            if(operator=="") {
+                input1 = setInput(input1,"3")
+            }else {
+                input2 = setInput(input2,"3")
+            }
         }
         buttonFour.setOnClickListener{
-            textViewResult.setText("4")
+            if(operator=="") {
+                input1 = setInput(input1,"4")
+            }else {
+                input2 = setInput(input2,"4")
+            }
         }
         buttonFive.setOnClickListener{
-            textViewResult.setText("5")
+            if(operator=="") {
+                input1 = setInput(input1,"5")
+            }else {
+                input2 = setInput(input2,"5")
+            }
         }
         buttonSix.setOnClickListener{
-            textViewResult.setText("6")
+            if(operator=="") {
+                input1 = setInput(input1,"6")
+            }else {
+                input2 = setInput(input2,"6")
+            }
         }
         buttonSeven.setOnClickListener{
-            textViewResult.setText("7")
+            if(operator=="") {
+                input1 = setInput(input1,"7")
+            }else {
+                input2 = setInput(input2,"7")
+            }
         }
         buttonEight.setOnClickListener{
-            textViewResult.setText("8")
+            if(operator=="") {
+                input1 = setInput(input1,"8")
+            }else {
+                input2 = setInput(input2,"8")
+            }
         }
         buttonNine.setOnClickListener{
-            textViewResult.setText("9")
+            if(operator=="") {
+                input1 = setInput(input1,"9")
+            }else {
+                input2 = setInput(input2,"9")
+            }
         }
         buttonZero.setOnClickListener{
-            textViewResult.setText("0")
+            if(operator=="") {
+                input1 = setInput(input1,"0")
+            }else {
+                input2 = setInput(input2,"0")
+            }
         }
+        buttonCancel.setOnClickListener{
+            input1 = ""
+            input2 = ""
+            operator = ""
+            result = ""
+            textViewResult.text=""
+        }
+        buttonPlus.setOnClickListener{
+            if(result!=""){
+                input1 = result
+                result = ""
+            }
+            operator = "+"
+        }
+        buttonEqual.setOnClickListener {
+            when (operator) {
+                "+" -> result = (input1.toInt() + input2.toInt()).toString()
+                "-" -> result = (input1.toInt() - input2.toInt()).toString()
+                "/" -> result = (input1.toInt() / input2.toInt()).toString()
+                "*" -> result = (input1.toInt() * input2.toInt()).toString()
+            }
+            textViewResult.text = result
+            input1 = ""
+            operator = ""
+            input2 = ""
+        }
+
     }
 
-    fun isInt(var1 : Any) : Boolean{
-        return var1 is Int
+    fun setInput(input:String, plus:String) :String{
+        var textViewResult : TextView = findViewById(R.id.calResult)
+        if(plus=="0" && input == ""){
+            textViewResult.text = ""
+            return ""
+        }else {
+            textViewResult.text = input + plus
+            return input + plus
+        }
     }
 }
