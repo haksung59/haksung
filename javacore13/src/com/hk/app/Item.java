fd6228996314e2b323eff660a5bb2b1f67af7446
KimHakSung <82303691+haksung59@users.noreply.github.com>
2022-05-22 22:19:01 +0900

    Delete javacore13 directory

--- javacore13/src/com/hk/app/Item.java
+++
@@ -1,9 +0,0 @@
-package com.hk.app;
-
-//인터페이스 : 상수 + 추상메소드로만 구성.
-public interface Item {
-	final static int Cnt = 10;
-	
-	public void useFastShoes();	// 한칸씩 이동 -> 3칸씩 이동.
-
-}
--- javacore13/src/com/hk/app/Weapon.java
+++
@@ -1,6 +0,0 @@
-package com.hk.app;
-
-public interface Weapon {
-
-	public void attack();
-}
