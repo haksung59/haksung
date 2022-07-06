6b0ccf1fc12b8900ae0873b7e8c6b6ff7bf07302
haksung59 <hak9501@gmail.com>
2022-07-06 22:50:43 +0900

    kopring

---
+++ kopring/src/main/kotlin/com/kopring/Repositories.kt
@@ -0,0 +1,8 @@
+package com.kopring
+
+import org.springframework.data.repository.CrudRepository
+import java.util.*
+
+interface MemberRepository:CrudRepository<Member?, String>{
+    override fun findById(id:String): Optional<Member?>
+}
--- kopring/src/main/kotlin/com/kopring/controller/HtmlController.kt
+++ kopring/src/main/kotlin/com/kopring/controller/HtmlController.kt
@@ -1,20 +1,96 @@
 package com.kopring.controller
 
+import com.kopring.Member
+import com.kopring.MemberRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.security.MessageDigest
+import javax.servlet.http.HttpSession
 
 @Controller
 class HtmlController {
 
+    @Autowired
+    lateinit var repository:MemberRepository
+
     @GetMapping("/")
     fun index(model : Model):String{
+        model.addAttribute("title", "Home")
         return "index"
     }
 
-    @GetMapping("/sign")
-    fun sign(model : Model):String{
-        return "sign"
+    fun crypto(ss:String):String{
+        val sha=MessageDigest.getInstance("SHA-256")
+        val hexa=sha.digest(ss.toByteArray())
+        return hexa.fold("", {str, it -> str + "%02x".format(it)})
+    }
+
+    @GetMapping("/{formType}")
+    fun signForm(model : Model, @PathVariable formType:String):String{
+        var response:String = ""
+        if(formType.equals("sign")){
+            response="sign"
+        }else if(formType.equals("login")){
+            response="login"
+        }
+
+        model.addAttribute("title", response)
+
+        return response
+    }
+
+    @PostMapping("/sign")
+    fun postSign(model:Model,
+                 @RequestParam(value="id")id:String,
+                 @RequestParam(value="password")pw:String,
+                 @RequestParam(value="name")name:String,
+                 @RequestParam(value="sex")sex:String) : String{
+        try {
+            //val cryptoPw=crypto(pw)
+            val member = repository.save(Member(id, pw, name, sex))
+        }catch(e:Exception){
+            e.printStackTrace()
+        }
+
+        model.addAttribute("title", "sign success")
+
+        return "index"
+    }
+
+    @GetMapping("/login/hi")
+    fun postLogin(model:Model,
+                  session:HttpSession,
+                 @RequestParam(value="id") id:String,
+                 @RequestParam(value="password") password:String):String{
+        var pageName:String=""
+        try{
+            //val cryptoPw = crypto(password)
+            val db_member =repository.findById(id).orElseThrow()
+
+            if(db_member != null){
+                val dbPw = db_member.pw
+                pageName = if(password == dbPw){
+                    session.setAttribute("id", db_member.id)
+                    model.addAttribute("title", "welcome")
+                    model.addAttribute("id", id)
+                    "welcome"
+                }else {
+                    model.addAttribute("title", "login")
+                    "login"
+                }
+            }
+
+        }catch(e:Exception){
+            e.printStackTrace()
+        }
+
+        return pageName
+
     }
 
 }
