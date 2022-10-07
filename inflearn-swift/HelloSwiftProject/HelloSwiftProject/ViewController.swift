d49a1f7dcfe5a9628a71f9929739d44e8c09e465
haksung59 <hak9501@gmail.com>
2022-10-07 15:59:29 +0900

    221007

--- inflearn-swift/HelloSwiftProject/HelloSwiftProject/ViewController.swift
+++ inflearn-swift/HelloSwiftProject/HelloSwiftProject/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var p1 = Point(x: 1.0, y: 2.0)
+    var p2 = Point(x: 2.0, y: 1.1)
+    
     @IBOutlet weak var lbl: UILabel!
-    @IBAction func changeLabel(_ sender: Any) {
-        lbl.text = "Hello, Swift"
+    @IBAction func changeLabel(_ sender: UIButton) {
+        if sender.currentTitle == "isEqual" {
+            if p1 == p2 {
+                lbl.text = "Equal"
+            }else {
+                lbl.text = "Not equal"
+            }
+        }else {
+            let p3 = p1++
+            lbl.text = String(format:"%.1f",p3.x) + ", " + String(format:"%.1f",p3.y)
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -19,6 +32,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
+
+extension Point:Equatable {
+    static func ==(lhs:Point, rhs:Point) -> Bool{
+        return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+    }
+}
+
+extension Point {
+    static prefix func -(pt:Point) -> Point{
+        return Point(x: -pt.x, y: -pt.y)
+    }
+}
+
+extension Point {
+    static postfix func ++(pt: inout Point) -> Point{
+        let ret = pt
+        pt.x += 1
+        pt.y += 1
+        return ret
+    }
+}
+
+extension Point{
+    static func +=(lhs: inout Point, rhs: Point) -> Point {
+        return Point(x:lhs.x+rhs.x, y:lhs.y+rhs.y)
+    }
+}
