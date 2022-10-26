3c5d1305393a6ba41bdaa1ace92d325c0c6d6061
haksung59 <hak9501@gmail.com>
2022-10-26 16:43:59 +0900

    20221026

--- healthcare/src/main/java/kr/or/test/healthcare/service/CoService.java
+++ healthcare/src/main/java/kr/or/test/healthcare/service/CoService.java
@@ -1,5 +1,6 @@
 package kr.or.test.healthcare.service;
 
+import com.google.gson.Gson;
 import kr.or.test.healthcare.mapper.CoMapper;
 import kr.or.test.healthcare.request.RequestPushTest;
 import kr.or.test.healthcare.request.RequestTest;
@@ -11,6 +12,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
 
 @Service
 public class CoService {
@@ -64,6 +71,55 @@ public class CoService {
     public Response pushTest(RequestPushTest request) throws Exception {
         startTime = System.currentTimeMillis();
         Response response = new Response();
+        request.setText(URLDecoder.decode(request.getText(), "UTF-8"));
+        if(request.getUri() != null){
+            request.setUri(URLDecoder.decode(request.getUri(), "UTF-8"));
+        }
+        String surl = "";
+
+        if(serverName.equals("dev") || serverName.equals("local")){
+            surl = "http://localhost:8080/" + serverName +"/push";
+        }else{
+            surl = "http://localhost:8080/push";
+        }
+
+        URL url = new URL(surl);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+//        conn.setRequestProperty("accKey", "~~~accKey~~~");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(request);
+
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+
+        try(OutputStream os = conn.getOutputStream()){
+            byte request_data[] = json.getBytes("utf-8");
+            os.write(request_data);
+        }
+
+        conn.connect();
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), "utf-8")
+        )){
+            StringBuilder result = new StringBuilder();
+            String responseLine = null;
+            while((responseLine = br.readLine()) != null){
+                result.append(responseLine.trim());
+            }
+            String strResult = String.valueOf(result);
+            response = gson.fromJson(strResult, Response.class);
+        }
+
+        response.setElapsedTime(startTime);
+
         return response;
     }
 
