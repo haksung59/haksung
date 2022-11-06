acdc04ec6612b64eedcffc8f51aa5e62e430efbf
haksung59 <hak9501@gmail.com>
2022-11-06 18:55:31 +0900

    221106 SwiftUI study

---
+++ SwiftUI/TutorialLandmarks/TutorialLandmarks/TutorialLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialLandmarksApp.swift
+//  TutorialLandmarks
+//
+//  Created by haksung on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct TutorialLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandmarkList()
+        }
+    }
+}
