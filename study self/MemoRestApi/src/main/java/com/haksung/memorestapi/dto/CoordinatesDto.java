7a168dc8b259e078520f75fe286da7c26a489930
haksung59 <hak9501@gmail.com>
2022-11-07 15:21:00 +0900

    modify RestApi for SwiftUI tutorial

--- study self/MemoRestApi/src/main/java/com/haksung/memorestapi/dto/CoordinatesDto.java
+++ study self/MemoRestApi/src/main/java/com/haksung/memorestapi/dto/CoordinatesDto.java
@@ -1,10 +1,13 @@
 package com.haksung.memorestapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CoordinatesDto {
 
+    @JsonIgnore
     int id;
 
     double longitude;
