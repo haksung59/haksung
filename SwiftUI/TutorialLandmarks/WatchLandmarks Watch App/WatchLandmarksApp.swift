46f09f47ca4d805112920a12ba12e8c5b6e99a71
haksung59 <hak9501@gmail.com>
2022-11-16 16:49:47 +0900

    delete

---
+++ SwiftUI/TutorialLandmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by haksung on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
