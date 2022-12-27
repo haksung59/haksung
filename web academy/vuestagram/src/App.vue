da271e8d2455a210eea28e26d3b52b5552fe5305
haksung59 <hak9501@gmail.com>
2022-12-27 18:27:32 +0900

    vue study 221227

--- web academy/vuestagram/src/App.vue
+++ web academy/vuestagram/src/App.vue
@@ -3,36 +3,89 @@
     <ul class="header-button-left">
       <li>Cancel</li>
     </ul>
-    <ul class="header-button-right">
-      <li>Next</li>
+    <ul v-if="showContent>0" class="header-button-right">
+      <li v-if="showContent==1" @click="showContent++">Next</li>
+      <li v-if="showContent==2" @click="publish()">발행</li>
     </ul>
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <ContainerBox :postData="postData"/>
+  <ContainerBox :postData="postData" :showContent="showContent"
+                :uploadImage="uploadImage" @write="writedContent=$event"/>
+  <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
+      <input @change="upload" accept="image/*" type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  <button @click="changeContent(0)">버튼0</button>
+  <button @click="changeContent(1)">버튼1</button>
+  <button @click="changeContent(2)">버튼2</button>
+
 </template>
 
 <script>
 import ContainerBox from './components/ContainerBox.vue';
-import PostData from './assets/data.json';
+import postData from './assets/data.json';
+import axios from 'axios';
 
 export default {
   name: 'App',
   data(){
     return {
-      postData: PostData,
-      postDataOriginal: [...PostData],
+      postData: postData,
+      postDataOriginal: [...postData],
+      clickTimes: 0,
+      showContent: 0,
+      uploadImage: null,
+      writedContent: "",
     }
   },
   components: {
-    ContainerBox,
+    ContainerBox
+  },
+  methods: {
+    more() {
+      this.clickTimes++
+      if (this.clickTimes % 2 == 1) {
+        axios.get("https://codingapple1.github.io/vue/more0.json")
+            .then((result) => {
+              this.postData.push(result.data);
+            })
+      }else {
+        axios.get("https://codingapple1.github.io/vue/more1.json")
+            .then((result) => {
+              this.postData.push(result.data);
+            })
+      }
+    },
+    changeContent(no){
+      this.showContent = no;
+    },
+    upload(e){
+      let files = e.target.files;
+      let url = URL.createObjectURL(files[0]);
+      this.uploadImage = url;
+      this.showContent = 1;
+    },
+    publish(){
+      var publishItem = {
+        "name": "Kim Hak Sung",
+        "userImage": "https://placeimg.com/100/100/arch",
+        "postImage": this.uploadImage ,
+        "likes": 0,
+        "date": "Today",
+        "liked": false,
+        "content": this.writedContent,
+        "filter": "Haksung59"
+      };
+      this.postData.unshift(publishItem);
+      this.showContent = 0;
+      this.uploadImage=null;
+    }
   }
 }
 </script>
