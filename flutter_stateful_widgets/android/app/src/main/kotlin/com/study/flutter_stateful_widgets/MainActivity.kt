3be9e3048bc6f0081bfd9407e91013f238955626
haksung <hak9501@gmail.com>
2023-03-02 18:08:51 +0900

    replace

--- flutter_stateful_widgets/android/app/src/main/kotlin/com/study/flutter_stateful_widgets/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.study.flutter_stateful_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- web academy/flutter_toonflix/android/app/src/main/kotlin/com/study/flutter_toonflix/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.study.flutter_toonflix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
