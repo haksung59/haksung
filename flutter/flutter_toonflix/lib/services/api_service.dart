5b00e6d2f69c49bf30ef213f2a11ee535f107742
haksung <hak9501@gmail.com>
2023-03-04 22:03:29 +0900

    flutter study http get통신 230304

--- flutter/flutter_toonflix/lib/services/api_service.dart
+++ flutter/flutter_toonflix/lib/services/api_service.dart
@@ -6,7 +6,9 @@ class ApiService {
   final String today = "today";
 
   void getTodaysToons() async {
-    final url = Uri.parse('$baseUrl/$today');
+    // final url = Uri.parse('$baseUrl/$today');
+    var url = Uri.parse(
+        'http://localhost:8080/api/v1/user/all/doubleCheck?userId=haksung59');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
