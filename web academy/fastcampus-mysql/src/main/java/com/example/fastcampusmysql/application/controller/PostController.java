68e1b5da3988bc67d3b2b0c93fc03e0bc3c8a9c0
haksung59 <hak9501@gmail.com>
2023-11-10 15:38:18 +0900

    fastcampus mysql study

--- web academy/fastcampus-mysql/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
+++ web academy/fastcampus-mysql/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.fastcampusmysql.application.controller;
 
+import com.example.fastcampusmysql.application.usecase.GetTimelinePostUsecase;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostCommand;
@@ -22,6 +23,7 @@ import java.util.List;
 public class PostController {
     final private PostWriteService postWriteService;
     final private PostReadService postReadService;
+    final private GetTimelinePostUsecase getTimelinePostUsecase;
 
     @PostMapping
     public Long create(PostCommand command) {
@@ -47,6 +49,14 @@ public class PostController {
             CursorRequest cursorRequest
     ) {
         return postReadService.getPosts(memberId, cursorRequest);
+    }
+
+    @GetMapping("members/{memberId}/timeline")
+    public PageCursor<Post> getTimelines(
+            @PathVariable Long memberId,
+            CursorRequest cursorRequest
+    ) {
+        return getTimelinePostUsecase.execute(memberId, cursorRequest);
     }
 
 }
--- web academy/fastcampus-mysql/src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ web academy/fastcampus-mysql/src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -101,6 +101,25 @@ public class PostRepository {
         return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
     }
 
+    public List<Post> findAllByMemberIdsAndOrderByIdDesc(List<Long> memberIds, int size) {
+        if(memberIds.isEmpty()){
+            return List.of();
+        }
+        var sql = String.format("""
+                SELECT *
+                FROM %s
+                WHERE memberId in (:memberIds)
+                ORDER BY id desc
+                LIMIT :size
+                """, TABLE);
+
+        var params = new MapSqlParameterSource()
+                .addValue("memberIds", memberIds)
+                .addValue("size", size);
+
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
+
     public List<Post> findAllByLessThanIdAndMemberIdAndOrderByIdDesc(Long id, Long memberId, int size) {
         var sql = String.format("""
                 SELECT *
@@ -111,6 +130,26 @@ public class PostRepository {
                 """, TABLE);
         var params = new MapSqlParameterSource()
                 .addValue("memberId", memberId)
+                .addValue("id", id)
+                .addValue("size", size);
+
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
+
+    public List<Post> findAllByLessThanIdAndMemberIdsAndOrderByIdDesc(Long id, List<Long> memberIds, int size) {
+        if(memberIds.isEmpty()){
+            return List.of();
+        }
+
+        var sql = String.format("""
+                SELECT *
+                FROM %s
+                WHERE memberId in (:memberIds) and id < :id
+                ORDER BY id desc
+                LIMIT :size
+                """, TABLE);
+        var params = new MapSqlParameterSource()
+                .addValue("memberIds", memberIds)
                 .addValue("id", id)
                 .addValue("size", size);
 
