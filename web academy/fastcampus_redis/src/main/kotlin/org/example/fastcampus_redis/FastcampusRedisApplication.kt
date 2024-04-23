5eb25b425e574c6dfd218851a896dcc665b79488
김학성 <hak9501@gmail.com>
2024-04-23 17:24:47 +0900

    create project

---
+++ web academy/fastcampus_redis/src/main/kotlin/org/example/fastcampus_redis/FastcampusRedisApplication.kt
@@ -0,0 +1,11 @@
+package org.example.fastcampus_redis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusRedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusRedisApplication>(*args)
+}
---
+++ web academy/fastcampus_redis/src/test/kotlin/org/example/fastcampus_redis/FastcampusRedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.fastcampus_redis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusRedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
